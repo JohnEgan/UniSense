64765beadbb10e16b812412ba5d0e87bf4c0e611
JohnEgan <jmegan1210@yahoo.com>
2022-01-10 19:43:10 -0500

    Update DualSenseHIDOutputReport.cs
    Test

--- UniSense/LowLevel/DualSenseHIDOutputReport.cs
+++ UniSense/LowLevel/DualSenseHIDOutputReport.cs
@@ -86,6 +86,7 @@ namespace UniSense.LowLevel
 
         public void SetMotorSpeeds(float lowFreq, float highFreq)
         {
+	    Debug.Log(HERE);
             flags1 |= Flags1.MainMotors1 | Flags1.MainMotors2;
             lowFrequencyMotorSpeed = (byte)Mathf.Clamp(lowFreq * 255, 0, 255);
             highFrequencyMotorSpeed = (byte)Mathf.Clamp(highFreq * 255, 0, 255);
